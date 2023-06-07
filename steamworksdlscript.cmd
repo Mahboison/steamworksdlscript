dfcc092f7734c317d2404a91bd1b98120d865ccc
Mahboison <55468715+Mahboison@users.noreply.github.com>
2023-06-07 11:48:09 -0400

    Update steamworksdlscript.cmd
    Fixed the mixed-up debug echos

--- steamworksdlscript.cmd
+++ steamworksdlscript.cmd
@@ -4,10 +4,10 @@ set /P mod=Enter workshop ID
 set /P game=Enter AppID
 cd C:\bin\steamcmd\ &::Make sure to change this to your SteamCMD install directory
 IF NOT DEFINED player (
-	echo "Logging in as user %player%"
+	echo "Logging in as anonymous"	
 	steamcmd.exe +login anonymous +workshop_download_item %game% %mod% +quit
 ) ELSE (
-	echo "Logging in as anonymous"
+	echo "Logging in as user %player%"
 	steamcmd.exe +login %player% +workshop_download_item %game% %mod% +quit
 )
 pause
