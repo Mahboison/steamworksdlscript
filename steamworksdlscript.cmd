9f453bb145c57824910f91f3809d66426a594a00
Mahboison <55468715+Mahboison@users.noreply.github.com>
2023-06-07 11:18:38 -0400

    Create steamworksdlscript.cmd
    The script in question

---
+++ steamworksdlscript.cmd
@@ -0,0 +1,13 @@
+@echo off
+set /P player=Login username (leave blank for anonymous) 
+set /P mod=Enter workshop ID 
+set /P game=Enter AppID
+cd C:\bin\steamcmd\
+IF NOT DEFINED player (
+	echo "Logging in as user %player%"
+	steamcmd.exe +login anonymous +workshop_download_item %game% %mod% +quit
+) ELSE (
+	echo "Logging in as anonymous"
+	steamcmd.exe +login %player% +workshop_download_item %game% %mod% +quit
+)
+pause
